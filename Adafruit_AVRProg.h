5748d2ba4493cadd28cb76a7c23c7d54ee9a6ae1
lady ada <limor@ladyada.net>
2021-09-28 16:23:24 -0400

    fix 51

--- Adafruit_AVRProg.h
+++ Adafruit_AVRProg.h
@@ -33,7 +33,7 @@ typedef struct image {
                        ///< datasheet!
 #if defined(__SAMD21G18A__) || defined(__SAMD21E18A__) ||                      \
     defined(TEENSYDUINO) || defined(ESP8266) || defined(ESP32) ||              \
-    defined(SAMD51)
+    defined(__SAMD51__)
   byte image_hexcode[50000]; ///< Max buffer for intel hex format image (text)
 #else
   byte image_hexcode[10000]; ///< Max buffer for intel hex format image (text)
